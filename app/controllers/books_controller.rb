b83ecde762f62ad153ea41abd6731b64a2222f4f
dmmyuki <104970837+dmmyuki@users.noreply.github.com>
2022-05-28 14:43:22 +0000

    [Create]4-exercise_0528

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,12 @@
 class BooksController < ApplicationController
   def index
+    @book=Book.new
+  end
+
+  def create
+    book=Book.new(book_params)
+    book.save
+    redirect_to books_path
   end
 
   def show
