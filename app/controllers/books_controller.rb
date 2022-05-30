1e1a451b88ad6ee135406dd5f8285b15d33cabfb
dmmyuki <104970837+dmmyuki@users.noreply.github.com>
2022-05-30 13:54:52 +0000

    [Create]4-exercise_0530

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,13 @@
 class BooksController < ApplicationController
   def index
-    @user=User.find(params[:id])
-    @books=@user.books
     @book=Book.new
+    @books=Book.all
   end
 
   def create
-    book=Book.new(book_params)
-    book.save
+    @book=Book.new(book_params)
+    @book.user_id=current_user.id
+    @book.save
     redirect_to books_path
   end
 
