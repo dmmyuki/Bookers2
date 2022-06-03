08ae80788f3e48fceb513ed91c617a43bc6fef6a
dmmyuki <104970837+dmmyuki@users.noreply.github.com>
2022-06-03 14:11:52 +0000

    [Create]4-exercise_0603

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,23 +5,25 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user=User.find(params[:id])
+    @user=current_user
+    @book=Book.find(params[:id])
     @books=@user.books
   end
 
   def edit
+    @user=User.find(params[:id])
   end
 
   def update
     user=User.find(params[:id])
     user.update(user_params)
-    redirect_to users_path(user.id)
+    redirect_to user_path(user.id)
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:name, :profile_image)
+    params.require(:user).permit(:name, :profile_image, :introduction)
   end
 
 end
