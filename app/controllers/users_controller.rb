a8ab4ca89af5ba4672e90c63a6134703efaf9bdf
dmmyuki <104970837+dmmyuki@users.noreply.github.com>
2022-06-04 14:57:33 +0000

    [Create]4-exercise_0604

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,12 @@
 class UsersController < ApplicationController
   def index
+    @book=Book.new
     @users=User.all
     @user=current_user
   end
 
   def show
+    @book=Book.new
     @user=current_user
     @book=Book.find(params[:id])
     @books=@user.books
@@ -15,9 +17,13 @@ class UsersController < ApplicationController
   end
 
   def update
-    user=User.find(params[:id])
-    user.update(user_params)
-    redirect_to user_path(user.id)
+    @user=User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice]="You have updated user successfully."
+      redirect_to user_path(user.id)
+    else
+      render:edit
+    end
   end
 
   private
